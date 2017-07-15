73cef43ef7713cadec5a7013954d4ffe20876aae
Comup <35429674@qq.com>
2017-07-16 02:07:21 +0800

    init

--- CEFSharpDemo/Form1.cs
+++ CEFSharpDemo/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,10 +13,28 @@ namespace CEFSharpDemo
 {
     public partial class Form1 : Form
     {
+        private ChromiumWebBrowser browser;
+
+        private TextBox address;
+
         public Form1()
         {
             InitializeComponent();
-            
+            var setting = new CefSettings();
+            setting.Locale = "zh-CN";
+            Cef.Initialize(setting, true, false);
+            browser = new ChromiumWebBrowser("https://www.baidu.com");
+            address = new TextBox();
+            address.KeyPress += Address_KeyPress;
+            Controls.Add(address);
+            Controls.Add(browser);
+        }
+
+        private void Address_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) {
+                browser.Load(address.Text);
+            }
         }
     }
 }
